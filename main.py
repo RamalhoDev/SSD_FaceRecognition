8db1fb3f76fa5af7b451820fe08a4cda67992e2b
Rodrigo <rodrigoramalho.dev@gmail.com>
2020-03-28 22:13:14 -0300

    Crop images

--- main.py
+++ main.py
@@ -2,20 +2,34 @@ from PIL import Image, ImageTk
 import numpy as np
 import os
 
-
-
 def getFFT(url):
     a = Image.open(url)
-    # a = Image.open("./orl_faces/s1/1.pgm")
     arr = np.array(a)
     fourizado = np.fft.fft2(arr)
-    fshift = np.fft.fftshift(fourizado.real)
-    magnitude_spectrum = 15*np.log(np.abs(fshift))
-    magnitude_spectrum = np.round(magnitude_spectrum)
-    return Image.fromarray(magnitude_spectrum.astype(int))
+    fshift = np.fft.fftshift(fourizado)
+    return fshift
 
-a = getFFT("./orl_faces/s1/1.pgm")
-a.show()
+def getSubRegion(image, newHeight, newWidth):
+    width, height = image.size
+    left = (width - newWidth)//2
+    right = (width + newWidth)//2
+    top = (height - newHeight)//2
+    bottom = (height + newHeight)//2
+
+    return image.crop((left,top,right,bottom))
+
+sizes = [2,4,6,10,12,15,20,25,30,30,50]
+a = getFFT("orl_faces/s1/1.pgm")
+a = Image.fromarray(a.astype(np.int32))
+
+cropImages = []
+
+for i in sizes:
+    b = getSubRegion(a, i, i)
+    cropImages.append(b)
+
+# width, height = b.size
+# print(f"{width} {height}")
 
 # images = []
 # for folder in os.listdir("./orl_faces"):
